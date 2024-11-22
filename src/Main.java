e6c13dfc71c2084371eb4331b172b612551017d1
Sergey Nikolenko <git.user.snikolenko.s.y@gmail.com>
2024-11-22 08:47:37 +0300

    добавлен функционал ввода чисел и математические операции над ними

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Введите два числа");
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Сумма введенныех чисел: " + (firstNumber + secondNumber));
+        System.out.println("Разность введенныех чисел: " + (firstNumber - secondNumber));
+        System.out.println("Поризведение введенныех чисел: " + (firstNumber * secondNumber));
+        System.out.println("Частное введенныех чисел: " + ((double)firstNumber / secondNumber));
     }
 }
