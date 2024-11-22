0f972e37ba7d6b0c3aad444b947681fbfb14575f
Sergey Nikolenko <git.user.snikolenko.s.y@gmail.com>
2024-11-22 08:51:54 +0300

    улудшение визуала

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
-        System.out.println("Введите два числа");
+        System.out.println("Введите первое число");
         int firstNumber = scanner.nextInt();
+        System.out.println("Введите второе число");
         int secondNumber = scanner.nextInt();
         scanner.close();
 
