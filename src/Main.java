4862d943caef163346366c8c271723c1294abd1c
Sergey Nikolenko <git.user.snikolenko.s.y@gmail.com>
2024-12-20 08:09:02 +0300

    Задания: 6.1 - Точка 6.2 - Линия 6.3 - Ломаная

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import OOP_6.Line;
-import OOP_6.Point;
-import OOP_6.PolyLine;
+import OOP_6.*;
+import OOP_7.Fraction;
 
 import java.util.Arrays;
 
@@ -20,21 +19,21 @@ public class Main {
 //-------------------------------
 
         // Задание 6.2 линия
-        Point start1 = new Point(1, 3);
-        Point end1 = new Point(5, 8);
-        Point start2 = new Point(10, 11);
-        Point end2 = new Point(15, 19);
-
-        Line l1 = new Line(start1, end1);
-        Line l2 = new Line(start2, end2);
-        Line l3 = new Line(end1, start2);
-
-        System.out.println("Координаты линии №3: " + l3);
-
-        end1.setPoint(99, 100);
-        start2.setPoint(200, 222);
-        System.out.println("Координаты линии №3 после изменений: " + l3);
-        sumLengthLine(l1,l2,l3);
+//        Point start1 = new Point(1, 3);
+//        Point end1 = new Point(5, 8);
+//        Point start2 = new Point(10, 11);
+//        Point end2 = new Point(15, 19);
+//
+//        Line l1 = new Line(start1, end1);
+//        Line l2 = new Line(start2, end2);
+//        Line l3 = new Line(end1, start2);
+//
+//        System.out.println("Координаты линии №3: " + l3);
+//
+//        end1.setPoint(99, 100);
+//        start2.setPoint(200, 222);
+//        System.out.println("Координаты линии №3 после изменений: " + l3);
+//        sumLengthLine(l1,l2,l3);
         //-------------------------------
 
 //        // Задание 6.3 Ломаная линия
@@ -54,8 +53,18 @@ public class Main {
 //        System.out.println("Массив линий входящих в ломаную: " + Arrays.toString(pl1.getLines()));
 //        System.out.println("Длина массива линий: " + pl1.getLengthLines());
 //        System.out.println(pl1.getLengthLines() == pl1.getLength());
+        //-------------------------------
+
+//        // Задание 7.1 Дробь
+
+        Fraction f1 = new Fraction(1, 3);
+        Fraction f2 = new Fraction(2, 5);
+        Fraction f3 = new Fraction(7, 8);
+
+        System.out.println(f1.sum(f2).sum(f3).minus(5));
 
     }
+
 
     public static void sumLengthLine(Line... line) {
         double sum = 0;
