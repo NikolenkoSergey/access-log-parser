bd8204ca7f54ae46e94d54e592a1c236949a5ce2
Sergey Nikolenko <git.user.snikolenko.s.y@gmail.com>
2025-01-23 18:29:36 +0300

    Число строк, maxLength, minLength свой класс исключеня LineTooLongException

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,9 +7,9 @@ public class Main {
         int n = 0;
         while (true) {
             System.out.print("Укажите путь к файлу: ");
-            String pach = new Scanner(System.in).nextLine();
+            String path = new Scanner(System.in).nextLine();
 
-            File file = new File(pach);
+            File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
 
@@ -22,6 +22,45 @@ public class Main {
             }
             n++;
             System.out.println("Путь указан верно! \nЭто файл номер " + n);
+
+
+            FileReader fileReader = null;
+            try {
+                fileReader = new FileReader(path);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            BufferedReader reader =
+                    new BufferedReader(fileReader);
+            String line;
+            int countLine = 0, maxLength = 0, minLength = Integer.MAX_VALUE;
+            while (true) {
+                try {
+                    if ((line = reader.readLine()) == null) break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                int length = line.length();
+                // Созданный класс исключения
+                if (length > 1024) throw new LineTooLongException("Длина строки превышает допустимое значение");
+
+                // Самая длинная строка
+                if (length > maxLength) {
+                    maxLength = length;
+                }
+                // Самая короткая строка
+                if (length < minLength) {
+                    minLength = length;
+                }
+                // число строк в файле
+                countLine++;
+            }
+            System.out.println("Число строк в файле: " + countLine);
+            System.out.println("Длина самой длинной строки в файле: " + maxLength);
+            System.out.println("Длина самой короткой строки в файле: " + minLength);
+
+//   C:\Users\snikolenko\Desktop\Курс по Java\access.log
         }
     }
 }
